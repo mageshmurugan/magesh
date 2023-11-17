fe7d4da1e733cea38553fd52b831d62aedf05c72
MageshMurugan <mageshmurugan64@gmail.com>
2023-11-17 22:35:05 +0530

    3rd commit

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
+"use client";
+import React,{useState} from 'react';
+import {HiMenu} from "react-icons/hi";
+import Link from 'next/link';
+import {RxCross2} from "react-icons/rx";
+import styles from "../page.module.css"
 const Nav = () => {
+    const [open, setOpen] = useState(false);
   return (
-    <>Mages
+    <>
+    <div> <h2>Magesh Murugan</h2></div>
+    <div>
+    <>
+        <HiMenu onClick={() => setOpen(true)} className={styles.navb} />
+      </>
+      {open && (
+        <div className={styles.navc} onClick={() => setOpen(false)}>
+            <div>
+                <RxCross2 onClick={() => setOpen(false)} className={styles.navb} />
+            </div>
+          <div className={styles.navca}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+        </div>
+      )}
+    </div>
     </>
   )
 }
